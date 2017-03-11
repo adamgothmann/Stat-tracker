789861ff9121dbd139f84b944049eb071d3984b1
adamgothmann <adamgothmann@gmail.com>
2017-03-10 23:02:03 -0600

    using jquery ajax

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,21 @@
 console.log('sourced');
+$(document).ready(function(){
+  console.log('jquery');
+
+  $.ajax({
+  type: "GET",
+  url: 'https://www.mysportsfeeds.com/api/feed/pull/nhl/2016-2017-regular/cumulative_player_stats.json?playerstats=G,A,Pts,Sh',
+  dataType: 'json',
+  async: false,
+  headers: {
+    "Authorization": "Basic " + btoa(username + ":" + password)
+  },
+  data: '{ "comment" }',
+  success: function(){
+    alert('Thanks for your comment!');
+  }
+});
+});
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
@@ -13,14 +30,16 @@ myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
     password = response.data.password;
   });
 
-  $http({
-    method: 'GET',
-    url: 'https://www.mysportsfeeds.com/api/feed/pull/nhl/current/cumulative_player_stats.json?playerstats=Pts',
-    headers: {
-      "Authorization": "Basic" + btoa(username + ":" + password)
-    }
-  }).then(function(response){
-    alert('it worked');
-    console.log(response);
-  });
+  // $http({
+  //   method: 'GET',
+  //   url: 'https://www.mysportsfeeds.com/api/feed/pull/nhl/current/cumulative_player_stats.json?playerstats=Pts',
+  //   headers: {
+  //     "Authorization": "Basic" + btoa(username + ":" + password)
+  //   }
+  // }).then(function(response){
+  //   alert('it worked');
+  //   console.log(response);
+  // });
+
+
 }]);
